fdf77de012dc0b32251fddc23c5bfcd104d94ee7
Ho Choi <cmarch314@gmail.com>
2015-12-28 21:30:34 -0800

    initializing

---
+++ FloorPlan/src/FP.java
@@ -0,0 +1,32 @@
+/*
+ * this class is Floor plan object
+ */
+import java.awt.*;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.*;
+
+
+public class FP {
+	private BufferedImage FloorPlanImage = null;
+	private int width;
+	private int height;
+	
+	/*
+	 * default constructor
+	 */
+	public FP()
+	{
+		
+	}
+	
+	
+	
+	
+	public boolean load()
+	{	
+		return false;
+	}
+	
+}
